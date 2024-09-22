8b5c59ede5bc0023c32d9a4ff870fb807c0fda56
aliyoub <aliyoub@gmail.com>
2024-09-22 23:13:17 +0200

    another commit

--- components/LongPressDoublePressComponent.tsx
+++ components/LongPressDoublePressComponent.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const LongPressDoublePressComponent = ({children}) => {
+const LongPressDoublePressComponent = ({children}:any) => {
   const [clicks, setClicks] = useState(0);
   const [isLongPress, setIsLongPress] = useState(false);
   const longPressDuration = 500; // Milliseconds
