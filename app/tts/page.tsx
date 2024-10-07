3775e9ce12d031b8b754d5beda2d9ee7a1c2e346
presta <event@gmail.com>
2024-10-07 05:38:14 +0200

    readind being fixed

--- app/tts/page.tsx
+++ app/tts/page.tsx
@@ -1,12 +1,21 @@
 'use client'
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TextToSpeech from './TextToSpeech'
 import { contents } from './contents';
 
 
 const Page = () => {
 const [text, setText] = useState("");
+
+useEffect(()=>{
+    if ('speechSynthesis' in window) {
+        alert("Speech synthesis is supported")
+      } else {
+        alert('Speech synthesis not supported on this device/browser.');
+      }
+      
+},[])
 
   return (
     <TextToSpeech text={contents} />
